015cef91f9cd542a21121b1c280c153c1e425fe1
15077693d <15077693d@gmail.com>
2020-07-25 17:28:29 +0800

    remove useless line

--- google_one_image.py
+++ google_one_image.py
@@ -12,7 +12,6 @@ def get_image(item):
         driver.quit()
         return images
     else:
-        image = ""
         driver.get(
             f'https://www.google.com.hk/search?q={item}&tbm=isch&ved=2ahUKEwi347PthejqAhUC6JQKHRAvCuUQ2-cCegQIABAA&oq=%E8%B3%88%E7%82%B3%E9%81%94%E9%81%93%E5%85%AC%E5%9C%92+%E8%B6%B3%E7%90%83%E5%A0%B4&gs_lcp=CgNpbWcQDFAAWABg5jVoAHAAeACAAQCIAQCSAQCYAQCqAQtnd3Mtd2l6LWltZw&sclient=img&ei=bfUbX_f4GYLQ0wSQ3qioDg&authuser=0&bih=1306&biw=1146&hl=zh-TW')
         element = driver.find_element_by_xpath('//img[@class="rg_i Q4LuWd"]')
